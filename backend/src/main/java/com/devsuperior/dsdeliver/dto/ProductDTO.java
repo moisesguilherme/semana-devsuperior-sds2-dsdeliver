ca10aa25ddec54c3a1db3fe8c546598c16c765b1
Moises Guilherme <moisescomp@gmail.com>
2021-01-05 21:06:16 -0300

    GET /Orders

--- backend/src/main/java/com/devsuperior/dsdeliver/dto/ProductDTO.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/dto/ProductDTO.java
@@ -8,7 +8,7 @@ public class ProductDTO implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 		
-	private Long id;
+	private Long id;	
 	private String name;
 	private Double price;
 	private String description;
