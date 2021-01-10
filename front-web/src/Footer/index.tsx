2240c9f75ed15368a99387c454864f6702653b1e
Moises Guilherme <moisescomp@gmail.com>
2021-01-09 21:59:13 -0300

    Add Fotter

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,29 @@
+import  './style.css'
+import { ReactComponent as YouTubeIcon } from './youtube.svg'
+import { ReactComponent as LinkedinIcon } from './linkedin.svg'
+import { ReactComponent as InstagramIcon } from './instagram.svg'
+
+
+import Home from '../Home';
+
+function Footer() {
+    return (
+       <footer className="main-footer">
+           App desenvolvido durante a segunda edição do evento Semana DevSuperior
+           <div className="footer-icons">
+               <a href="https://www.youtube.com/c/DevSuperior" target="_new">
+                    <YouTubeIcon/>
+               </a>
+               <a href="https://www.linkedin.com/school/devsuperior" target="_new">
+                    <LinkedinIcon/>
+               </a>
+
+               <a href="https://www.instagram.com/devsuperior.ig/">
+                    <InstagramIcon />
+               </a>
+           </div>
+       </footer>
+    )
+}
+
+export default Footer;
